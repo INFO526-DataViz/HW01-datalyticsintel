567706267a7ba02b4c5a922617abaca7c6f29617
Antonio <antonio@datalyticsintel.com>
2023-09-04 19:55:33 -0700

    skipping 1 for now as I need to figure out r verision of the package. made histogram for beginning of 2

--- HW01-datalyticsintel.R
+++ HW01-datalyticsintel.R
@@ -8,20 +8,36 @@ toc: true
 ## 1 - Road traffic accidents in Edinburgh
 #load librabries gglplot, here, tidyverse
 #install and then Load library dsbox packages called accidents
-# install.packages("accidents")
-library(tidyverse)
+install.packages("accidents")
+# install.packages("dsbox")
+install.packages("dsbox")
+ library(tidyverse)
  # library(accidents)
   
   `{r label-me-1}`
 
 ## 2 - NYC marathon winners
+  
 # Install openintro
-# install.packages("openintro")
+install.packages("openintro")
 # load library openintro
 library(openintro)
-
+library(ggplot2)
+# a. Create a histogram and a box plot of the distribution of marathon times of 
+# all runners in the dataset. What features of the distribution are apparent in the histogram 
+# and not the box plot? What features are apparent in the box plot but not in the histogram?
 # ```{r label-me-2}
 
+ggplot(nyc_marathon, aes(x = time)) +
+  geom_histogram() +
+  labs(title = "Histogram of NYC Marathon Times", x = "Time (minutes)", y = "Number of Runners") 
+  
+
+
+
+  
+  
+  
 # ```
 
 ## 3 - US counties
